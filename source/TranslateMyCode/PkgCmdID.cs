c6a606200ff4a144c77373efdafd6521290d569e
Tomi Niemenmaa <tomi.niemenmaa@evolvit.fi>
2015-11-17 11:47:12 +0200

    Initial Commit

---
+++ source/TranslateMyCode/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace TomiNiemenmaa.TranslateMyCode
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdTranslate =        0x100;
+
+
+    };
+}
